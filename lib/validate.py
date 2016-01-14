aad359ca6808a4350c0dd79a6e102d6b53a928ed
Sunz3r <Sunz3r@users.noreply.github.com>
2016-01-14 13:41:08 +0100

    validate node_id
    if the node_id contains invalid characters then rrd-update interrupt the backend-script

--- lib/validate.py
+++ lib/validate.py
@@ -16,4 +16,13 @@ def validate_nodeinfo(nodeinfo):
         if 'latitude' not in nodeinfo['location'] or 'longitude' not in nodeinfo['location']:
             return False
 
+    if 'node_id' in nodeinfo:
+        # Allow node_id from MAC without ":"
+        # Allow node_id like "gw1"
+        if re.search(r'[^a-zA-Z0-9]', nodeinfo['node_id']):
+            return False
+    else:
+        # Reject if node_id is missing
+        return False
+
     return True
